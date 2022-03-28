04a8921a2c02d9fb09af383cfc3531191a84be64
Andrea <andrea@yaaaw.it>
2022-03-29 00:30:11 +0200

    feat(asserts): version 1.0.0-alpha

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# asserts
+# TypeScript asserts
+
+An npm package with useful assert functions.
+
+List of functions:
+
+- `assertNotNull`
+
+  ```typescript
+  import { assertNotNull } from "asserts";
+  ```
+
+  Assert that a value is not null
+
+  ```typescript
+  const user: User | null = await fetchUser(endpoint);
+
+  assertNotNull(user);
+
+  // now we are sure that `user` is of User type
+  const userId = user.id;
+  ```
+
+- `assertNever`
+
+  ```typescript
+  import { assertNever } from "asserts";
+  ```
+
+  Ensure that all union variants are checked
+
+  ```typescript
+  type UnionA = {
+    message: "unionA";
+  };
+
+  type UnionB = {
+    message: "unionB";
+  };
+
+  type UnionC = UnionA | UnionB;
+
+  const aStrangeConstant: UnionC = aStrangeFunction();
+
+  switch (aStrangeConstant.message) {
+    case "unionA": {
+      console.log("I found UnionA");
+      break;
+    }
+
+    case "unionB": {
+      console.log("I found UnionB");
+    }
+
+    default: {
+      assertNever(aStrangeConstant);
+    }
+  }
+  ```
---
+++ index.ts
@@ -0,0 +1,67 @@
+type MessageOption = {
+  message: string;
+};
+
+type ErrorOption = {
+  error: Error;
+};
+
+type Option = MessageOption | ErrorOption;
+
+/**
+ * assert that a value is not null
+ *
+ * ```typescript
+ * const user: User | null = await fetchUser(endpoint)
+ *
+ * assertNotNull(user, { error: new UserNotFound(endpoint) })
+ *
+ * // now `user` is of type `User`
+ * console.log(user)
+ * ```
+ *
+ * @param value
+ * @param option an optional message or an optional error
+ * @return assert that value is T, throw if null
+ */
+export function assertNotNull<T>(
+  value: T | null,
+  option?: Option
+): asserts value is T {
+  if (option == null) {
+    if (value == null) {
+      throw new Error();
+    }
+    return;
+  }
+
+  if ("message" in option) {
+    if (value == null) {
+      throw new Error(option.message);
+    }
+    return;
+  }
+
+  if (value == null) {
+    throw option.error;
+  }
+}
+
+/**
+ * assert that all variants of an union have been exhausted
+ *
+ * @param value
+ * @param option an optional message or an optional error
+ * @return never return, just throw an error
+ */
+export function assertNever(value: never, option?: Option): never {
+  if (option == null) {
+    throw new Error(`Unexpected value: ${value}`);
+  }
+
+  if ("message" in option) {
+    throw new Error(option.message);
+  }
+
+  throw option.error;
+}
